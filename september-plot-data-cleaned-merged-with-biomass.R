297bb8300e8559cbc61383b4a3884306d4dc152f
jxliu123 <73325328+jxliu123@users.noreply.github.com>
2020-11-05 23:45:40 -0500

    Update september-plot-data-cleaned-merged-with-biomass.R

--- september-plot-data-cleaned-merged-with-biomass.R
+++ september-plot-data-cleaned-merged-with-biomass.R
@@ -7,9 +7,9 @@ library(tidyverse)
 # import data and coerce a the data frame to a tibble 
 # with as_tibble("file name"), replace "file name" with
 # the name of the file in your library
-library(tidyverse)
-library(readxl)
-X20200923_SeptemberPlotData_v1 <- read_excel("raw data/20200923_SeptemberPlotData_v1.xlsx")
+
+
+X20200923_SeptemberPlotData_v1 <- read_csv("raw data/20200923_SeptemberPlotData_v1.xlsx")
 as_tibble("X20200923_SeptemberPlotData_v1")
 
 
@@ -23,28 +23,28 @@ X20200923_SeptemberPlotData_v1<-X20200923_SeptemberPlotData_v1 %>%filter(!is.na(
 
 
 # the column names are strings and so are 
-# the corresponding data points
+# the corresponding data points have to convert to integers so they can be compared with the Biomass data 
 
 
 
 # import biomass data and coerce data frame as a tibble
-library(readxl)
-X20200927_BiomassPlotData <- read_excel("raw data/20200927_BiomassPlotData.xlsx")
+
+X20200927_BiomassPlotData <- read_csv("raw data/20200927_BiomassPlotData.xlsx")
 as_tibble(X20200927_BiomassPlotData)
 
 
 
 # rearrange columns so that block, plot, and plant 
 # are the first three columns 
-col_order<- c("Block", "Plot", "Plant", "Species", "Mass")
-X20200927_BiomassPlotData <- X20200927_BiomassPlotData[,col_order]
+
+X20200927_BiomassPlotData <- select(X20200927_BiomassPlotData, Block, Plot, Plant, Species, Mass)
 
 #removes data points with missing information
 X20200927_BiomassPlotData<- X20200927_BiomassPlotData%>%filter(!is.na(Mass))
 view(X20200927_BiomassPlotData)
 
 
-SeptemberBiomassMerge<-merge(X20200923_SeptemberPlotData_v1,X20200927_BiomassPlotData)
+SeptemberBiomassMerge<-left_join(X20200923_SeptemberPlotData_v1,X20200927_BiomassPlotData, by = Block, Plot, Plant, Species)
 view(SeptemberBiomassMerge)
 
 
